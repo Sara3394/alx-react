5726943694236f6cf848d18bed4f754938dfba31
Sara <Sara3394@github.com>
2023-04-17 17:11:53 -0700

    tasks

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   mode: 'production',
@@ -10,3 +10,4 @@ module.exports = {
     filename: 'bundle.js',
   },
 };
+
