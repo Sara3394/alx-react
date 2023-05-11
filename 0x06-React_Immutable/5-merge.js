66635f511b335beca21fd38d3c638c7fe14c5a3b
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-11 13:45:39 -0700

    Update 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,6 @@
 import { List, Map } from 'immutable';
+//uses fromJS from the Immutable.js library
 
-export default function concatElements(page1, page2) {
-    return List(page1).concat(page2);
-}
+export const concatElements = (page1, page2) => List(page1).concat(page2);
 
-export default function mergeElements(page1, page2) {
-    return Map(page1).merge(page2);
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(page2);
