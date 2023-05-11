b73f44ecb34bf2096e93fc048d6a57dde922869f
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-11 14:29:51 -0700

    Update 7-equality.js

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,5 +1,5 @@
 import { Map, is } from "immutable";
-//uses fromJS from the Immutable.js library
+//uses Map and is from the Immutable.js library
 
 export default function areMapsEqual(map1, map2) {
   return is(map1, map2);
