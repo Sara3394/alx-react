926ff5040703951ee60e8f098f8e9944fdd28be4
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-11 13:47:01 -0700

    Update 6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,1 +1,6 @@
+import { Map } from "immutable";
+//uses fromJS from the Immutable.js library
 
+export default function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
+}
