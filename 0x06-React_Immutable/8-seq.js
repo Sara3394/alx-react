d0e0087a6c62babacacec8658b48e86a52fd4d00
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-11 14:21:10 -0700

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,21 +1,21 @@
-import immutable from 'immutable';
+import immutable from "immutable";
 //uses fromJS from the Immutable.js library
-
-const firstLetterCap = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 const { Seq } = immutable;
 
-export default function printBestStudents(objects) {
-  const bestStudents = Seq(objects)
+const makeUppercase = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export default function printBestStudents(object) {
+  const bestStudents = Seq(object)
     .filter((student) => student.score > 70)
     .map((student) => {
       const { firstName, lastName } = student;
       return {
         ...student,
-        firstName: firstLetterCap(firstName),
-        lastName: firstLetterCap(lastName),
+        firstName: makeUppercase(firstName),
+        lastName: makeUppercase(lastName),
       };
     });
-  
+
   bestStudents.forEach((student) => console.log(student));
 }
