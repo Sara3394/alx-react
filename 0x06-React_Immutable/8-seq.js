33e156a6bce8791e9b6a89ee6036e76857531d0c
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-11 14:33:24 -0700

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,24 +1,22 @@
-import { Seq } from "immutable";
-//uses fromJS from the Immutable.js library
+import { Seq } from 'immutable';
+//uses Seq from the Immutable.js library
 
-export default function printBestStudents(grades) {
+const printBestStudents = (grades) => {
   const seq = Seq(grades);
 
-  const filtered = seq.filter((student) => {
-    student.firstName.charAt(0).toUpperCase();
-    return student.score > 70;
+  const getStudent = seq.filter((value) => value.score > 70);
+
+  const student = getStudent.toJS();
+
+  const firstCap = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+  Object.keys(student).map((key) => {
+    student[key].firstName = firstCap(student[key].firstName);
+    student[key].lastName = firstCap(student[key].lastName);
+    return student[key];
   });
 
-  function capFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  console.log(student);
+};
 
-  const JSObject = filtered.toJS();
-
-  Object.keys(JSObject).map((key) => {
-    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
-    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
-    return JSObject[key];
-  });
-
-  console.log(JSObject);
+export default printBestStudents;
