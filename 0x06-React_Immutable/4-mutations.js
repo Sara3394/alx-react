46de48d7e0f187292152611d05f3e3792710645f
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-11 13:16:18 -0700

    Update 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,1 +1,15 @@
+import { Map } from "immutable";
+//uses fromJS from the Immutable.js library
 
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+export const map2 = map.withMutations((valuesModified) => {
+  valuesModified.set(2, 'Benjamin').set(4, 'Oliver');
+});
