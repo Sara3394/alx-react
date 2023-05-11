0e1c458cf7c5036bda9afe5e924d2cdce701567b
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2023-05-11 12:29:35 -0700

    Update 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,1 +1,7 @@
+import { fromJS } from "immutable";
+//uses fromJS from the Immutable.js library
 
+export default function accessImmutableObject(object, array) {
+  const mapObj = fromJS(object);
+  return mapObj.getIn(array, undefined);
+}
